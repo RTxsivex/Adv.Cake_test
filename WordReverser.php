e8252f52279f6d9823dd444d329fcbca01be3eb2
RTxsivex <rtangelx@gmail.com>
2025-09-30 14:51:06 +0300

    ready

--- WordReverser.php
+++ WordReverser.php
@@ -6,7 +6,7 @@ class WordReverser
 {
     public static function reverse(string $text): string
     {
-        // Разбиваем на токены (слова + пробелы)
+        
         $tokens = preg_split('/(\s+)/u', $text, -1, PREG_SPLIT_DELIM_CAPTURE);
         
         return implode('', array_map([self::class, 'processToken'], $tokens));
@@ -18,7 +18,6 @@ class WordReverser
             return $token;
         }
 
-        // В токене переворачиваем каждую буквенную последовательность отдельно
         return preg_replace_callback('/\p{L}+/u', [self::class, 'reverseWord'], $token);
     }
 
@@ -27,7 +26,6 @@ class WordReverser
         $word = $match[0];
         $chars = mb_str_split($word, 1, 'UTF-8');
         
-        // ЗАПОМИНАЕМ: какие позиции были в верхнем регистре
         $upperPositions = [];
         foreach ($chars as $i => $char) {
             if (self::isUpperCase($char)) {
@@ -35,17 +33,17 @@ class WordReverser
             }
         }
         
-        // Переворачиваем все буквы в нижнем регистре
+        
         $lowerChars = array_map(function($char) {
             return mb_strtolower($char, 'UTF-8');
         }, $chars);
         
         $reversed = array_reverse($lowerChars);
         
-        // Восстанавливаем верхний регистр на ТЕХ ЖЕ ПОЗИЦИЯХ, где он был originally
+        
         $result = '';
         foreach ($reversed as $i => $char) {
-            // Если эта позиция в исходном слове была в верхнем регистре
+            
             if (in_array($i, $upperPositions)) {
                 $result .= mb_strtoupper($char, 'UTF-8');
             } else {
